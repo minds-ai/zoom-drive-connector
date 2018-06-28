84ed19df693e32c01888f20ff97b3e31536cacb3
MrFlynn <nick@minds.ai>
2018-06-28 16:02:49 -0700

    Altered DriveConfig, SystemConfig, and ConfigInterface.
    DriveConfig now properly structured to store correct values as required
    by the Drive API. Formatting fixes in SystemConfig and ConfigInterface.
    SystemConfig now has __class__ method for returning class name.

--- configuration/configuration_interfaces.py
+++ configuration/configuration_interfaces.py
@@ -74,30 +74,35 @@ class ZoomConfig(APIConfigBase):
 
 
 class DriveConfig(APIConfigBase):
-  def __init__(self, key: str, secret: str, folder_id: str):
-    """Initializes key, secret, and folder ID values for Google Drive API. Passes `self.key` and
-        `self.secret` to APIConfigBase.
+  def __init__(self, credentials: str, secret: str, folder_id: str):
+    """Initializes credentials file, secrets file, and folder ID values for Google Drive API. Passes
+    `self.secret` to APIConfigBase.
 
-        :param key: Drive API client key.
-        :param secret: Drive API client secret.
-        :param folder_id: ID of folder to upload recordings to.
-        """
-    super().__init__(key, secret)
+    :param credentials: File used by Drive API to authenticate client.
+    :param secret: Drive API client secret file.
+    :param folder_id: ID of folder to upload recordings to.
+    """
+    super().__init__("", secret)
+    self.credentials = credentials
     self.folder_id = folder_id
 
   def validate(self) -> bool:
-    """Checks to see if key, secret, and folder IDs are not empty.
+    """Checks to see if all parameters are valid.
 
-        :return: Secret, key, and folder_id have length of at least one.
-        """
-    return super().validate() and len(self.folder_id) > 0
+    :return: Checks to make sure that the secret file exists and the folder ID is not empty or
+    otherwise invalid.
+    """
+    files_exist = os.path.exists(self.secret)
+    valid_folder_id = self.folder_id is not None and len(self.folder_id) > 0
+
+    return files_exist and valid_folder_id
 
   @staticmethod
   def __class__():
     """Implements __class__ property.
 
-        :return: Name of class
-        """
+    :return: Name of class
+    """
     return 'Drive'
 
 
@@ -105,24 +110,28 @@ class SystemConfig:
   def __init__(self, target_folder: str, port: int):
     """Initializes download folder and port attributes.
 
-        :param target_folder: folder to download temporary recordings.
-        :param port: port on which to run the application.
-        """
+    :param target_folder: folder to download temporary recordings.
+    :param port: port on which to run the application.
+    """
     self.target_folder = target_folder
     self.port = port
 
   def validate(self) -> bool:
     """Returns true if the target folder exists and the port number is greater than 1000.
 
-        :return: True if the conditions listed about evaluate individually to true.
-        """
+    :return: True if the conditions listed about evaluate individually to true.
+    """
     return os.path.isdir(self.target_folder) and self.port > 1000
+
+  @staticmethod
+  def __class__():
+    return 'System'
 
 
 class ConfigInterface:
   def __init__(self, file: str):
     """Initializes and loads configuration file to Python object.
-        """
+    """
     self.file = file
     self.config_tuple = None
 
@@ -131,8 +140,8 @@ class ConfigInterface:
 
   def __load_config(self) -> dict:
     """Loads YAML configuration file to Python object. Does some basic error checking to help
-        with debugging bad configuration files.
-        """
+    with debugging bad configuration files.
+    """
     try:
       with open(self.file, 'r') as f:
         return yaml.safe_load(f)
@@ -147,24 +156,31 @@ class ConfigInterface:
 
   def __create_interfaces(self):
     """Loads configuration file using `self.__load_config` and generates tuple containing
-        classes specific to each top level section.
-        """
+    classes specific to each top level section.
+    """
     conf_dict = self.__load_config()
 
     zoom = conf_dict["zoom"]
-    print(zoom['meetings'])
-    self.zoom_conf = ZoomConfig(zoom['key'], zoom['secret'], zoom['username'], zoom['password'],
-                                zoom['delete'], zoom['meetings'])
+    self.zoom_conf = ZoomConfig(zoom['key'],
+                                zoom['secret'],
+                                zoom['username'],
+                                zoom['password'],
+                                zoom['delete'],
+                                zoom['meetings'])
+
     drive = conf_dict["drive"]
-    self.drive_conf = DriveConfig(drive['credentials_json'], drive['client_secret_json'],
+    self.drive_conf = DriveConfig(drive['credentials_json'],
+                                  drive['client_secret_json'],
                                   drive['folder_id'])
     slack = conf_dict["slack"]
-    self.slack_conf = SlackConfig(slack["key"], slack["channel"])
+    self.slack_conf = SlackConfig(slack["key"],
+                                  slack["channel"])
 
-    local = conf_dict["internals"]
-    self.local_conf = SystemConfig(local['target_folder'], local['port'])
+    system = conf_dict["internals"]
+    self.local_conf = SystemConfig(system['target_folder'],
+                                   system['port'])
 
-    conf_tuple = (self.zoom_conf, self.drive_conf)  # local_conf slack_conf
+    conf_tuple = (self.zoom_conf, self.drive_conf, self.slack_conf, self.local_conf)
 
     for c in conf_tuple:
       # Validate each item in `conf_tuple`.
