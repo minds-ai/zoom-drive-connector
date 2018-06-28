649e1cdc6b5700b9900d38f1e6806c8da5aa4dc8
MrFlynn <nick@minds.ai>
2018-06-28 15:38:10 -0700

    Altered Google Drive interface to use configuration storage classes.

--- drive/drive_api.py
+++ drive/drive_api.py
@@ -4,27 +4,22 @@ import httplib2
 import apiclient
 
 from oauth2client import file, client, tools
+
 from drive import DriveAPIException
+
+from configuration import DriveConfig
+from configuration import SystemConfig
 
 
 class DriveAPI:
-  def __init__(self, base_path: str, credentials: str, secrets: str, folder_id: str):
+  def __init__(self, drive_config: DriveConfig, sys_config: SystemConfig):
     """Initializes instance of DriveAPI class.
 
-    :param base_path: Path to temporary folder where recordings will be stored.
-    :param credentials: Path to credentials.json.
-    :param secrets: Path to secrets.json.
-    :param folder_id: Target Google Drive folder ID to upload recordings to.
+    :param drive_config: configuration class containing all parameters needed for Google Drive.
+    :param sys_config: configuration class containing all system related parameters.
     """
-    if base_path.endswith('/'):
-      # Remove any trailing forward slashes in path to prevent path errors.
-      self._base_path = base_path[:-1]
-    else:
-      self._base_path = base_path
-
-    self._credentials = credentials
-    self._secrets = secrets
-    self._folder_id = folder_id
+    self.drive_config = drive_config
+    self.sys_config = sys_config
 
     self._scopes = ['https://www.googleapis.com/auth/drive.file']
     self._service = None
@@ -35,11 +30,11 @@ class DriveAPI:
     """Triggers the OAuth2 setup flow for Google API endpoints. Requires the ability to open
     a link within a web browser in order to work.
     """
-    store = file.Storage(self._credentials)
+    store = file.Storage(self.drive_config.credentials)
     creds = store.get()
 
     if not creds or creds.invalid:
-      flow = client.flow_from_clientsecrets(self._secrets, self._scopes)
+      flow = client.flow_from_clientsecrets(self.drive_config.secret, self._scopes)
       creds = tools.run_flow(flow, store)
 
     self._service = apiclient.discovery.build('drive', 'v3', http=creds.authorize(httplib2.Http()))
@@ -56,14 +51,14 @@ class DriveAPI:
       # Raise an exception if setup() hasn't been run.
       raise DriveAPIException(name="Service error", reason="setup() method not called.")
 
-    complete_path = '{0}/{1}'.format(self._base_path, filename)
+    complete_path = '{0}/{1}'.format(self.sys_config.target_folder, filename)
     if not filename or not os.path.exists(complete_path):
       # Raise an exception if the specified file doesn't exist.
       raise DriveAPIException(
           name="File error", reason='{f} could not be found.'.format(f=complete_path))
 
     # Google Drive file metadata
-    metadata = {'name': name, 'parents': [self._folder_id]}
+    metadata = {'name': name, 'parents': [self.drive_config.folder_id]}
 
     # Create a new upload of the recording and execute it.
     media = apiclient.http.MediaFileUpload(complete_path, mimetype='video/mp4')
