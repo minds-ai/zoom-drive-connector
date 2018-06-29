df2499dc6cf1245b9e3c1f221f6404b32f68d7cc
MrFlynn <nick@minds.ai>
2018-06-28 17:42:15 -0700

    Updated shell scripts.
    style_check.sh can now be run over specified files, directories, or
    the whole project depending on the argument given. Both scripts have
    shebang at the top of the file.

--- style_check.sh
+++ style_check.sh
@@ -1,3 +1,62 @@
-pycodestyle --config=.pycodestyle $1
-pylint --rcfile=".pylintrc" --output-format=parseable $1
+#!/usr/bin/env bash
 
+#######################################
+# Runs pycodestyle and pylint against a
+# specified file.
+# Globals:
+#   None
+# Arguments:
+#   file_name: name of file to run checks
+#              on.
+# Returns:
+#   None
+#######################################
+check_file () {
+  # Function arguments.
+  local file_name="$1"
+
+  echo "File: $file_name"
+  echo "PYCODESTYLE:"
+  pycodestyle --config=.pycodestyle "$file_name"
+
+  echo "PYLINT:"
+  pylint --rcfile=".pylintrc" --output-format=parseable "$file_name"
+}
+
+#######################################
+# Gets an array of files in the given
+# directory and in all subdirectories
+# and runs check_file against them.
+# Globals:
+#   None
+# Arguments:
+#   folder: name of folder to check files
+#           in.
+# Returns:
+#   None
+#######################################
+check_files_in_dir () {
+  # Function arguments.
+  local folder="$1"
+
+  # Get array of files.
+  IFS=" " read -ra PY_FILES <<< "$(find "$folder" -type f -iname "*.py" -exec echo {} +)"
+
+  # Run checks against all files in array.
+  for file in "${PY_FILES[@]}"; do
+    check_file "$file"
+  done
+ }
+
+if [[ $# -lt 1 ]]; then
+  # If no argument is provided then check all files in all directories.
+  check_files_in_dir .
+else
+  if [[ -f $1 ]]; then
+    # Check specific file.
+    check_file "$1"
+  else
+    # Check specific folder.
+    check_files_in_dir "$1"
+  fi
+fi
