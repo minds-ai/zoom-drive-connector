a9ecfa92d7a1e47fc01d89cf21c3120d1ee24e9b
Jeroen Bédorf <jeroen@bedorf.net>
2018-11-02 00:15:12 +0100

    Fix URL location and exception handling

--- zoom/zoom_api.py
+++ zoom/zoom_api.py
@@ -14,6 +14,7 @@
 # ==============================================================================
 
 import datetime
+from enum import Enum
 import os
 import time
 import shutil
@@ -30,6 +31,12 @@ log = logging.getLogger('app')
 S = TypeVar("S", bound=APIConfigBase)
 
 
+class ZoomURLS(Enum):
+  recordings = 'https://api.zoom.us/v2/meetings/{id}/recordings'
+  delete_recordings = 'https://api.zoom.us/v2/meetings/{id}/recordings/{rid}'
+  signin = 'https://api.zoom.us/signin'
+
+
 class ZoomAPI:
   def __init__(self, zoom_config: S, sys_config: S):
     """Class initialization; sets client key, secret, and download folder path.
@@ -41,8 +48,15 @@ class ZoomAPI:
     self.sys_config = cast(SystemConfig, sys_config)
 
     # Information required to login to allow downloads
-    self.zoom_signin_url = 'https://api.zoom.us/signin'
+    self.zoom_signin_url = ZoomURLS.signin.value
     self.timeout = 1800  # Default expiration time is 30 minutes.
+
+    # Clarified HTTP status messages
+    self.message = {
+        401: 'Not authenticated.',
+        404: 'File not found or no recordings',
+        409: 'File deleted already.'
+    }
 
   def generate_jwt(self) -> bytes:
     """Generates the JSON web token used for authenticating with Zoom. Sends client key
@@ -51,8 +65,7 @@ class ZoomAPI:
     payload = {'iss': self.zoom_config.key, 'exp': int(time.time()) + self.timeout}
     return jwt.encode(payload, str(self.zoom_config.secret), algorithm='HS256')
 
-  @staticmethod
-  def delete_recording(meeting_id: str, recording_id: str, auth: bytes):
+  def delete_recording(self, meeting_id: str, recording_id: str, auth: bytes):
     """Given a specific meeting room ID and recording ID, this function moves the recording to the
     trash in Zoom's cloud.
 
@@ -60,17 +73,14 @@ class ZoomAPI:
     :param recording_id: The ID of the recording to trash.
     :param auth: JWT token.
     """
-    zoom_url = 'https://api.zoom.us/v2/meetings/{id}/recordings/{rid}'.format(
-        id=meeting_id, rid=recording_id)
+    zoom_url = str(ZoomURLS.delete_recordings.value).format(id=meeting_id, rid=recording_id)
     res = requests.delete(
         zoom_url, params={'access_token': auth,
                           'action': 'trash'})  # trash, not delete
-    if res.status_code == 401:
-      # Handle unauthenticated requests.
-      raise ZoomAPIException(401, 'Unauthorized', res.request, 'Not authenticated.')
-    elif res.status_code == 409:
-      # Handle error where content may have been removed already.
-      raise ZoomAPIException(409, 'Resource Conflict', res.request, 'File deleted already.')
+    status_code = res.status_code
+    if 400 <= status_code <= 499:
+      raise ZoomAPIException(status_code, res.reason, res.request, self.message.get(
+          status_code, ''))
 
   def get_recording_url(self, meeting_id: str, auth: bytes) -> dict:
     """Given a specific meeting room ID and auth token, this function gets the download url
@@ -80,7 +90,7 @@ class ZoomAPI:
     :param auth: Encoded JWT authorization token
     :return: dict containing the date of the recording, the ID of the recording, and the video url.
     """
-    zoom_url = 'https://api.zoom.us/v2/meetings/{id}/recordings'.format(id=meeting_id)
+    zoom_url = str(ZoomURLS.recordings.value).format(id=meeting_id)
 
     try:
       zoom_request = requests.get(zoom_url, params={'access_token': auth})
@@ -89,13 +99,9 @@ class ZoomAPI:
       # but print an additional warning in case it was a configuration error
       log.log(logging.ERROR, e)
       raise ZoomAPIException(404, 'File Not Found', None, 'Could not connect')
-    if zoom_request.status_code == 401:
-      # Handle unauthenticated requests.
-      raise ZoomAPIException(401, 'Unauthorized', zoom_request.request, 'Not authenticated.')
-    elif zoom_request.status_code == 404:  # You get this if there are no files
-      raise ZoomAPIException(404, 'File Not Found', zoom_request.request,
-                             'File not found or no recordings')
-    else:
+
+    status_code = zoom_request.status_code
+    if 200 <= status_code <= 299:
       for req in zoom_request.json()['recording_files']:
         # TODO(jbedorf): For now just delete the chat messages and continue processing other files.
         if req['file_type'] == 'CHAT':
@@ -103,8 +109,14 @@ class ZoomAPI:
         elif req['file_type'] == 'MP4':
           date = datetime.datetime.strptime(req['recording_start'], '%Y-%m-%dT%H:%M:%SZ')
           return {'date': date, 'id': req['id'], 'url': req['download_url']}
+      # Raise 404 when we do not recognize the file type
       raise ZoomAPIException(404, 'File Not Found', zoom_request.request,
                              'File not found or no recordings')
+    elif 300 <= status_code <= 599:
+      raise ZoomAPIException(status_code, zoom_request.reason, zoom_request.request,
+                             self.message.get(status_code, ''))
+    else:
+      raise ZoomAPIException(status_code, zoom_request.reason, zoom_request.request, '')
 
   def download_recording(self, url: str) -> str:
     """Downloads video file from Zoom to local folder.
