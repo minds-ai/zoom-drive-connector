39f9f119e2cf3165efcc4e96818cfe802ecd2bbe
MrFlynn <nick@minds.ai>
2018-11-03 21:38:51 -0700

    Added more complete suite of unit tests for Zoom API.

--- tests/test_zoom.py
+++ tests/test_zoom.py
@@ -1,11 +1,15 @@
-import unittest
 import datetime
+import time
+
+import unittest
+from unittest.mock import MagicMock
+
 import responses
 import zoom
-import time
 import jwt
 
 from configuration import ZoomConfig, SystemConfig
+
 
 class TestZoom(unittest.TestCase):
   sys_config = SystemConfig({'target_folder': '/tmp'})
@@ -21,11 +25,25 @@ class TestZoom(unittest.TestCase):
   api = zoom.ZoomAPI(zoom_config, sys_config)
 
   def test_generate_jwt(self):
-    token = jwt.encode({'iss': self.zoom_config.key, 'exp': int(time.time() + 1800)},
-                       str(self.zoom_config.secret),
-                       algorithm='HS256')
+    good_token = jwt.encode({'iss': self.zoom_config.key, 'exp': int(time.time() + 1800)},
+                            str(self.zoom_config.secret),
+                            algorithm='HS256')
 
-    self.assertEqual(self.api.generate_jwt(), token)
+    self.assertEqual(self.api.generate_jwt(), good_token)
+
+    bad_token = jwt.encode({'iss': 'fake', 'exp': int(time.time())},
+                           str(self.zoom_config.secret),
+                           algorithm='HS256')
+
+    self.assertNotEqual(self.api.generate_jwt(), bad_token)
+
+  @responses.activate
+  def test_delete_recording_errors(self):
+    responses.add(responses.DELETE, 'https://api.zoom.us/v2/meetings/some-meeting/recordings/rid',
+                  status=404)
+
+    with self.assertRaises(zoom.ZoomAPIException):
+      self.api.delete_recording('some-meeting', 'rid', b'token')
 
   @responses.activate
   def test_get_url_success(self):
@@ -40,8 +58,8 @@ class TestZoom(unittest.TestCase):
 
     self.assertEqual(self.api.get_recording_url('some-meeting-id', b'token'),
                      {'date': datetime.datetime(2018, 1, 1, 1, 1, 1),
-                       'id': 'some-recording-id',
-                       'url': 'https://example.com/download'}
+                      'id': 'some-recording-id',
+                      'url': 'https://example.com/download'}
                      )
 
   @responses.activate
@@ -63,3 +81,34 @@ class TestZoom(unittest.TestCase):
 
     with self.assertRaises(zoom.ZoomAPIException):
       self.api.get_recording_url('some-meeting-id', b'token')
+
+  @responses.activate
+  def test_downloading_file(self):
+    pass
+    # TODO: Not really sure how to test a streaming file download.
+    # TODO(nick): This method needs to have more rigorous exception checking.
+
+  @responses.activate
+  def test_handling_zoom_errors_file_pull(self):
+    responses.add(responses.GET, 'https://api.zoom.us/v2/meetings/some-meeting-id/recordings',
+                  status=404)
+
+    self.assertEqual(self.api.pull_file_from_zoom('some-meeting-id', True),
+                     {'success': False, 'date': None, 'filename': None})
+
+    # TODO: handle calling delete method. However, this relies on previous test being completed.
+
+  @responses.activate
+  def test_filesystem_errors_file_pull(self):
+    responses.add(responses.GET, 'https://api.zoom.us/v2/meetings/some-meeting-id/recordings',
+                  status=200,
+                  json={'recording_files': [{
+                    'file_type': 'MP4',
+                    'recording_start': '2018-01-01T01:01:01Z',
+                    'download_url': 'https://example.com/download',
+                    'id': 'some-recording-id'
+                  }]})
+
+    self.api.download_recording = MagicMock(side_effect=OSError('Could not write file!'))
+    self.assertEqual(self.api.pull_file_from_zoom('some-meeting-id', True),
+                     {'success': False, 'date': None, 'filename': None})
